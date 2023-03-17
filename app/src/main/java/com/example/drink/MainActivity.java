216c7e8f75717836de828c1dc21533d3bc3af6a7
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2023-03-17 14:21:31 +0100

    pulsante per dati biometrici e logica iniziale

--- app/src/main/java/com/example/drink/MainActivity.java
+++ app/src/main/java/com/example/drink/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.drink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.biometrics.*;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private Button buttonBiometricAuth = null;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        buttonBiometricAuth = (Button) findViewById(R.id.biometricbuttom);
+
+        buttonBiometricAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO inserire logica login con dati biometrici
+                promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                        .setTitle("Biometric login for my app")
+                        .setSubtitle("Log in using your biometric credential")
+                        .setAllowedAuthenticators(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)
+                        .build();
+            }
+        });
     }
 }
